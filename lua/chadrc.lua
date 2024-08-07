324ab72a097022cf6ab53a804dee00bf92ba7967
Hyeonki Hong <hhk7734@gmail.com>
2024-08-08 03:40:03 +0900

    feat: set theme to github_dark

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,11 +1,11 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
+	theme = "github_dark",
 
 	-- hl_override = {
 	-- 	Comment = { italic = true },
