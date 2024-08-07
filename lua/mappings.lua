362a16a36a278bfb550bc41e80108e59337ad407
Hyeonki Hong <hhk7734@gmail.com>
2024-08-08 03:57:28 +0900

    feat: move keymap to lua/mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("i", "<Tab>", "<C-t>", { desc = "add indent" })
+map("i", "<S-Tab>", "<C-d>", { desc = "remove indent" })
