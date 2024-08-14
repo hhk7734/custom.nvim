85651fc1f1d61502a214b298c326df9c2e789c06
Hyeonki Hong <hhk7734@gmail.com>
2024-08-15 01:54:21 +0900

    feat: change comment and function.method hl

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,12 +5,11 @@
 local M = {}
 
 M.base46 = {
-	theme = "github_dark",
-
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  theme = "github_dark",
+  hl_override = {
+    ["@comment"] = { fg = "#00aa1c" },
+    ["@function.method"] = { bold = true },
+  },
 }
 
 return M
