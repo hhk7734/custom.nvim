7cb2b4fa78e5504890189d8dbeb42cb22d164ffb
Hyeonki Hong <hhk7734@gmail.com>
2025-11-02 03:08:42 +0900

    feat: add tokyonight

--- lua/lazy-config.lua
+++ lua/lazy-config.lua
@@ -33,7 +33,7 @@ require("lazy").setup({
 
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
+  install = { colorscheme = { "tokyonight" } },
 
   ui = {
     icons = {
