a0de87fff6d73662002d8ad41855cd8e43f68c03
Hyeonki Hong <hhk7734@gmail.com>
2025-11-02 21:55:47 +0900

    feat: add lualine

---
+++ lua/lazy-plugins/lualine.lua
@@ -0,0 +1,12 @@
+return {
+  -- https://github.com/nvim-lualine/lualine.nvim
+  "nvim-lualine/lualine.nvim",
+
+  lazy = false,
+
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+
+  opts = {
+    theme = "palenight",
+  },
+}
