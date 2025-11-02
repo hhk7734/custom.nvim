afb8d3701e87284baa7d7aa4104e3d448f521fab
Hyeonki Hong <hhk7734@gmail.com>
2025-11-02 20:46:45 +0900

    feat: add which-key

---
+++ lua/lazy-plugins/which-key.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/which-key.nvim",
+
+  -- Lazy-load on keypresses.
+  keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+
+  -- Lazy-load on commands.
+  cmd = { "WhichKey" },
+
+  opts = {},
+}
